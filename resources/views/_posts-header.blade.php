46e91efb2bce09e9fad352dd780f4fc945e1b7e4
Udayanga <udayanga.wijesiri@gmail.com>
2024-01-14 11:51:09 +0530

    Extracted a Dropdown component

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -18,24 +18,22 @@
             <x-slot name="trigger">
                 <button class="py-2 pl-3 pr-9 text-sm font-semibold text-left inline-flex w-full">
                     {{isset($currentCategory) ? ucwords($currentCategory->name) : 'Categories'}}
-                    <svg class="transform -rotate-90 absolute pointer-events-none" style="right: 12px;" width="22"
-                    height="22" viewBox="0 0 22 22">
-                        <g fill="none" fill-rule="evenodd">
-                            <path stroke="#000" stroke-opacity=".012" stroke-width=".5" d="M21 1v20.16H.84V1z">
-                            </path>
-                            <path fill="#222"
-                                    d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z"></path>
-                        </g>
-                    </svg>
+                    <x-icon name="down-arrow" class="absolute pointer-events-none" style="right: 12px;"/>
                 </button>
             </x-slot>
                 
 
-            {{-- Below is the default slot, no need to name it --}}
-            <a href="/" class="block text-left px-3 text-sm leading-5 hover:bg-blue-500 focus:bg-blue-500 hover:text-white focus:text-white">All</a> 
+            {{-- Below (x-dropdown-item and foreach) is the default slot referring to dropdown.blade.php, no need to name it --}}
+            <x-dropdown-item href="/" :active="request()->routeIs('home')">
+                All
+            </x-dropdown-item>
 
             @foreach ($categories as $category)
-                <a href="/categories/{{$category->slug}}" class="block text-left px-3 text-sm leading-5 hover:bg-blue-500 focus:bg-blue-500 hover:text-white focus:text-white {{isset($currentCategory) && $category->id === $currentCategory->id ? 'bg-blue-500 text-white' : ''}}">{{ucwords($category->name)}}</a> 
+                <x-dropdown-item 
+                    href="/categories/{{$category->slug}}" 
+                    :active='request()->is("categories/{$category->slug}")'
+                    >{{ucwords($category->name)}}
+                </x-dropdown-item>
             @endforeach    
         </x-dropdown>
       </div>
