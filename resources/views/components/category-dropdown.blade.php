1637a264c25478ea02c68ee25154107ef42eadfe
Udayanga <udayanga.wijesiri@gmail.com>
2024-01-16 09:34:46 +0530

    Extract a Category Dropdown Component and move post view files to post directory

---
+++ resources/views/components/category-dropdown.blade.php
@@ -0,0 +1,24 @@
+<div>
+    <x-dropdown>
+        <x-slot name="trigger">
+            <button class="py-2 pl-3 pr-9 text-sm font-semibold text-left inline-flex w-full">
+                {{isset($currentCategory) ? ucwords($currentCategory->name) : 'Categories'}}
+                <x-icon name="down-arrow" class="absolute pointer-events-none" style="right: 12px;"/>
+            </button>
+        </x-slot>
+            
+
+        {{-- Below (x-dropdown-item and foreach) is the default slot referring to dropdown.blade.php, no need to name it --}}
+        <x-dropdown-item href="/" :active="request()->routeIs('home')">
+            All
+        </x-dropdown-item>
+
+        @foreach ($categories as $category)
+            <x-dropdown-item 
+                href="/?category={{$category->slug}}" 
+                :active='request()->is("categories/{$category->slug}")'
+                >{{ucwords($category->name)}}
+            </x-dropdown-item>
+        @endforeach    
+    </x-dropdown>
+</div>
