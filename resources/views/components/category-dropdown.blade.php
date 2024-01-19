a3bf65374d129d173afbedbabeba9fa3cf97bf19
Udayanga <udayanga.wijesiri@gmail.com>
2024-01-19 10:10:56 +0530

    Add pagination

--- resources/views/components/category-dropdown.blade.php
+++ resources/views/components/category-dropdown.blade.php
@@ -2,20 +2,21 @@
     <x-dropdown>
         <x-slot name="trigger">
             <button class="py-2 pl-3 pr-9 text-sm font-semibold text-left inline-flex w-full">
-                {{isset($currentCategory) ? ucwords($currentCategory->name) : 'Categories'}}
+                {{ isset($currentCategory) ? ucwords($currentCategory->name) : 'Categories' }}
                 <x-icon name="down-arrow" class="absolute pointer-events-none" style="right: 12px;"/>
             </button>
         </x-slot>
             
 
         {{-- Below (x-dropdown-item and foreach) is the default slot referring to dropdown.blade.php, no need to name it --}}
-        <x-dropdown-item href="/" :active="request()->routeIs('home')">
+        <x-dropdown-item href="/?{{ http_build_query(request()->except('category', 'page')) }}"
+              :active="request()->routeIs('home')">
             All
         </x-dropdown-item>
 
         @foreach ($categories as $category)
             <x-dropdown-item 
-                href="/?category={{ $category->slug }}&{{ http_build_query(request()->except('category')) }}" 
+                href="/?category={{ $category->slug }}&{{ http_build_query(request()->except('category', 'page')) }}" 
                 :active='request()->is("categories/{$category->slug}")'
                 >{{ ucwords($category->name) }}
             </x-dropdown-item>
