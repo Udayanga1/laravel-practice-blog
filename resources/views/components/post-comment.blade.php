50b5e30daaab2537bb63c5207c868db2d8ba3f26
Udayanga <udayanga.wijesiri@gmail.com>
2024-01-24 08:15:36 +0530

    Markup for a Post comment

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,17 @@
+<article class="flex space-x-4 border border-gray-200 rounded-xl bg-gray-100 p-6">
+  <div class="flex-shrink-0">
+    <img src="https://i.pravatar.cc/60" alt="" width="60" height="60" class="rounded-xl">
+  </div>
+  <div>
+    <header class="mb-4">
+      <h3 class="font-bold">John Doe</h3>
+      <p class="text-xs">
+        Posted
+        <time>8 months ago</time>
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae, molestias.
+      </p>
+    </header>
+  </div>
+</article>
