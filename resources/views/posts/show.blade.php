7fd5c72f3029971e6390cfd16aba3c77fde34928
Udayanga <udayanga.wijesiri@gmail.com>
2024-01-26 09:59:52 +0530

    Activate the comment form

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -52,24 +52,32 @@
 									</div>
 							</div>
 							<section class="col-span-8 col-start-5 mt-10 space-y-6">
-								<x-panel>
-									<form action="#" method="POST">
-										@csrf
-	
-										<header class="flex items-center">
-											<img src="https://i.pravatar.cc/40?u={{ auth()->id() }}" alt="" width="40" height="40" class="rounded-full">
-											<h2 class="ml-4">Want to participate?</h2>
-	
-										</header>
-										<div class="mt-6">
-												<textarea name="body" id="body" class="w-full text-sm focus:outline-none focus:ring p-1" rows="5" placeholder="Quick, thing of something to say!" required></textarea>
-										</div>
-										<div class="flex justify-end mt-10 border-t border-gray-200">
-											<button class="bg-blue-500 text-white uppercase font-semibold text-xs py-2 px-10 mt-6 rounded-2xl hover:bg-blue-600">Post</button>
-										</div>
-	
-									</form>
-								</x-panel>
+								@auth
+									<x-panel>
+										<form action="/posts/{{ $post->slug }}/comments" method="POST">
+											@csrf
+		
+											<header class="flex items-center">
+												<img src="https://i.pravatar.cc/40?u={{ auth()->id() }}" alt="" width="40" height="40" class="rounded-full">
+												<h2 class="ml-4">Want to participate?</h2>
+		
+											</header>
+											<div class="mt-6">
+													<textarea name="body" id="body" class="w-full text-sm focus:outline-none focus:ring p-1" rows="5" placeholder="Quick, thing of something to say!" required></textarea>
+											</div>
+											<div class="flex justify-end mt-10 border-t border-gray-200">
+												<button class="bg-blue-500 text-white uppercase font-semibold text-xs py-2 px-10 mt-6 rounded-2xl hover:bg-blue-600">Post</button>
+											</div>
+		
+										</form>
+									</x-panel>
+								
+								@else
+									<p>
+										<a href="/register" class="font-semibold text-blue-500 hover:underline">Register</a> or <a href="/login" class="font-semibold text-blue-500 hover:underline">Log in</a> to leave a comments
+									</p>
+
+								@endauth
 
 								@foreach ($post->comments as $comment)
 									<x-post-comment :comment="$comment" />
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,10 @@
 <?php
 
-use App\Models\Post;
-use App\Models\User;
-use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
+use App\Http\Controllers\PostCommentsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,7 +18,9 @@ use App\Http\Controllers\SessionsController;
 */
 
 Route::get('/', [PostController::class, 'index'])->name('home');
+
 Route::get('/posts/{post:slug}', [PostController::class, 'show']);
+Route::post('posts/{post:slug}/comments', [PostCommentsController::class, 'store']);
 
 Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
 Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
@@ -29,3 +29,7 @@ Route::get('login', [SessionsController::class, 'create'])->middleware('guest');
 Route::post('sessions', [SessionsController::class, 'store'])->middleware('guest');
 
 Route::post('logout', [SessionsController::class, 'destroy'])->middleware('auth');
+
+
+// 7 restful actions
+// index, show, create, store, edit, update, destroy
